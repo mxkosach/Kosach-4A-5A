0b84ae078ffe75ca6f291e46614a0d3510a82f8c
Maksim Kosach <98594836+mxkosach@users.noreply.github.com>
2022-05-25 23:16:43 +0300

    Add files via upload

--- src/main/java/by/bsu/lab4a/main/Main.java
+++ src/main/java/by/bsu/lab4a/main/Main.java
@@ -2,6 +2,7 @@ package by.bsu.lab4a.main;
 
 import by.bsu.lab4a.action.PuppyOperator;
 import by.bsu.lab4a.entity.Puppy;
+import by.bsu.lab4a.exception.FileException;
 import by.bsu.lab4a.reader.DataReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,7 +12,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
     static Logger logger= LogManager.getLogger();
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException,FileException {
         //Puppy puppy=new Puppy("Charlie",2,POODLE);
         DataReader dataReader=new DataReader();
         Puppy puppy=new Puppy();
